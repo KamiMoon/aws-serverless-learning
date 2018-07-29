ba9bc34b5bc8417bf205677c9f896b5a11e2e7c7
Eric Kizaki <erickizaki@gmail.com>
2018-07-28 21:14:06 -0500

    master add webpack/babel/eslint

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+const url = 'http://checkip.amazonaws.com/';
+
+export const handler = async (event, context, callback) => {
+    let response;
+
+    try {
+        const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello world',
+                location: ret.data.trim()
+            })
+        }
+    }
+    catch (err) {
+        callback(err, null);
+    }
+
+    callback(null, response)
+};
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    target: 'node', // in order to ignore built-in modules like path, fs, etc.
+    externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        library: "[name]",
+        libraryTarget: "commonjs2",
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            // First, run the linter.
+            // It's important to do this before Babel processes the JS.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+                options: {
+                    quiet: true,
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+};
